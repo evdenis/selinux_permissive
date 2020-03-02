806706bb297ecd6fcb57068967963d43e752f3f8
Denis Efremov <efremov@linux.com>
2020-03-02 23:07:43 +0300

    service.sh: delay switch to boot complition

--- service.sh
+++ service.sh
@@ -1,6 +1,22 @@
-if [ -x "$(command -v setenforce)" ]
+# Schedule task
+
+nohup /bin/sh > /dev/null 2>&1 <<EOF &
+
+wait_boot_complete()
+{
+	until [ "x\$(getprop sys.boot_completed)" == "x1" ]
+	do
+		sleep 5
+	done
+}
+
+wait_boot_complete
+
+if [ -x "\$(command -v setenforce)" ]
 then
 	setenforce 0
 else
 	echo -n 0 > /sys/fs/selinux/enforce
 fi
+
+EOF
